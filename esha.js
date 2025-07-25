04f74d753c2633e12dfd5526e395e96ea147a33c
esha <mahmudalhakim223@gmail.com>
2025-07-26 00:35:42 +0700

    initial commit

--- esha.js
+++ esha.js
@@ -19,3 +19,15 @@
         button.addEventListener('mouseleave', removePressedClass);
     });
 })();
+const scrollReveal = () => {
+    const sections = document.querySelectorAll('.section');
+    const options = {threshold: 0.1};
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {if (entry.isIntersecting) {entry.target.classList.add('visible');}});
+    }, options);
+    sections.forEach(section => {
+        section.classList.add('fade-in');
+        observer.observe(section);
+    });
+}
+scrollReveal();
